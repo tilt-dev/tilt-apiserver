094c2ffcc65af1b899b294ad427211c384a983e8
Nick Santos <nicholas.j.santos@gmail.com>
2021-02-16 11:15:23 -0500

    storage: add a system to store API objects in memory (#14)

--- pkg/storage/filepath/jsonfile_test.go
+++ pkg/storage/filepath/jsonfile_test.go
@@ -2,8 +2,10 @@ package filepath_test
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,86 +24,58 @@ import (
 type Manifest = v1alpha1.Manifest
 type ManifestList = v1alpha1.ManifestList
 
-func TestReadEmpty(t *testing.T) {
-	f := newFixture(t)
-	defer f.TearDown()
+func fileSystems() []filepath.FS {
+	return []filepath.FS{filepath.RealFS{}, filepath.NewMemoryFS()}
+}
 
-	_, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
-	if assert.Error(t, err) {
-		assert.True(t, apierrors.IsNotFound(err))
+func TestReadEmpty(t *testing.T) {
+	for _, fs := range fileSystems() {
+		t.Run(fmt.Sprintf("%T", fs), func(t *testing.T) {
+			f := newFixture(t, fs)
+			defer f.TearDown()
+			f.TestReadEmpty()
+		})
 	}
 }
 
 func TestCreateThenRead(t *testing.T) {
-	f := newFixture(t)
-	defer f.TearDown()
-
-	_, err := f.storage.Create(context.Background(), &Manifest{
-		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
-	}, nil, &metav1.CreateOptions{})
-	require.NoError(t, err)
-
-	obj, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
-	require.NoError(t, err)
-
-	createdAt := obj.(*Manifest).ObjectMeta.CreationTimestamp.Time
-	assert.True(t, !createdAt.IsZero())
+	for _, fs := range fileSystems() {
+		t.Run(fmt.Sprintf("%T", fs), func(t *testing.T) {
+			f := newFixture(t, fs)
+			defer f.TearDown()
+			f.TestCreateThenRead()
+		})
+	}
 }
 
 func TestCreateThenList(t *testing.T) {
-	f := newFixture(t)
-	defer f.TearDown()
-
-	obj, err := f.storage.List(context.Background(), nil)
-	require.NoError(t, err)
-
-	manifestList := obj.(*ManifestList)
-	assert.Equal(t, 0, len(manifestList.Items))
-
-	_, err = f.storage.Create(context.Background(), &Manifest{
-		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
-	}, nil, &metav1.CreateOptions{})
-	require.NoError(t, err)
-
-	obj, err = f.storage.List(context.Background(), nil)
-	require.NoError(t, err)
-
-	manifestList = obj.(*ManifestList)
-	assert.Equal(t, 1, len(manifestList.Items))
+	for _, fs := range fileSystems() {
+		t.Run(fmt.Sprintf("%T", fs), func(t *testing.T) {
+			f := newFixture(t, fs)
+			defer f.TearDown()
+			f.TestCreateThenList()
+		})
+	}
 }
 
 func TestCreateThenReadThenDelete(t *testing.T) {
-	f := newFixture(t)
-	defer f.TearDown()
-
-	_, err := f.storage.Create(context.Background(), &Manifest{
-		TypeMeta:   metav1.TypeMeta{Kind: "Manifest", APIVersion: "core.tilt.dev/v1alpha1"},
-		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
-	}, nil, &metav1.CreateOptions{})
-	require.NoError(t, err)
-
-	obj, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
-	require.NoError(t, err)
-
-	createdAt := obj.(*Manifest).ObjectMeta.CreationTimestamp.Time
-	assert.True(t, !createdAt.IsZero())
-
-	_, _, err = f.storage.Delete(context.Background(), "my-manifest", nil, &metav1.DeleteOptions{})
-	require.NoError(t, err)
-
-	_, err = f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
-	if assert.Error(t, err) {
-		assert.True(t, apierrors.IsNotFound(err))
+	for _, fs := range fileSystems() {
+		t.Run(fmt.Sprintf("%T", fs), func(t *testing.T) {
+			f := newFixture(t, fs)
+			defer f.TearDown()
+			f.TestCreateThenReadThenDelete()
+		})
 	}
 }
 
 type fixture struct {
+	t       *testing.T
 	dir     string
 	storage rest.StandardStorage
 }
 
-func newFixture(t *testing.T) *fixture {
-	dir, err := ioutil.TempDir("", t.Name())
+func newFixture(t *testing.T, fs filepath.FS) *fixture {
+	dir, err := ioutil.TempDir("", strings.Replace(t.Name(), "/", "_", -1))
 	require.NoError(t, err)
 
 	scheme := runtime.NewScheme()
@@ -117,13 +91,75 @@ func newFixture(t *testing.T) *fixture {
 		},
 	}
 
-	provider := filepath.NewJSONFilepathStorageProvider(&Manifest{}, dir, filepath.RealFS{})
+	provider := filepath.NewJSONFilepathStorageProvider(&Manifest{}, dir, fs)
 	storage, err := provider(scheme, options)
 	require.NoError(t, err)
 
 	return &fixture{
+		t:       t,
 		dir:     dir,
 		storage: storage.(rest.StandardStorage),
+	}
+}
+
+func (f *fixture) TestReadEmpty() {
+	_, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
+	if assert.Error(f.t, err) {
+		assert.True(f.t, apierrors.IsNotFound(err))
+	}
+}
+
+func (f *fixture) TestCreateThenRead() {
+	_, err := f.storage.Create(context.Background(), &Manifest{
+		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
+	}, nil, &metav1.CreateOptions{})
+	require.NoError(f.t, err)
+
+	obj, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
+	require.NoError(f.t, err)
+
+	createdAt := obj.(*Manifest).ObjectMeta.CreationTimestamp.Time
+	assert.True(f.t, !createdAt.IsZero())
+}
+
+func (f *fixture) TestCreateThenList() {
+	obj, err := f.storage.List(context.Background(), nil)
+	require.NoError(f.t, err)
+
+	manifestList := obj.(*ManifestList)
+	assert.Equal(f.t, 0, len(manifestList.Items))
+
+	_, err = f.storage.Create(context.Background(), &Manifest{
+		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
+	}, nil, &metav1.CreateOptions{})
+	require.NoError(f.t, err)
+
+	obj, err = f.storage.List(context.Background(), nil)
+	require.NoError(f.t, err)
+
+	manifestList = obj.(*ManifestList)
+	assert.Equal(f.t, 1, len(manifestList.Items))
+}
+
+func (f *fixture) TestCreateThenReadThenDelete() {
+	_, err := f.storage.Create(context.Background(), &Manifest{
+		TypeMeta:   metav1.TypeMeta{Kind: "Manifest", APIVersion: "core.tilt.dev/v1alpha1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "my-manifest"},
+	}, nil, &metav1.CreateOptions{})
+	require.NoError(f.t, err)
+
+	obj, err := f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
+	require.NoError(f.t, err)
+
+	createdAt := obj.(*Manifest).ObjectMeta.CreationTimestamp.Time
+	assert.True(f.t, !createdAt.IsZero())
+
+	_, _, err = f.storage.Delete(context.Background(), "my-manifest", nil, &metav1.DeleteOptions{})
+	require.NoError(f.t, err)
+
+	_, err = f.storage.Get(context.Background(), "my-manifest", &metav1.GetOptions{})
+	if assert.Error(f.t, err) {
+		assert.True(f.t, apierrors.IsNotFound(err))
 	}
 }
 
