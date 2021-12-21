bacf5d3a313cdc0a4ae2cf0c8dfb80b0dc69b017
Nick Santos <nick@tilt.dev>
2021-12-21 12:27:06 -0500

    vendor: upgrade k8s, controller-runtime (#55)

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,4 +7,4 @@ cd "$DIR/.."
 
 exec docker run -v "$(pwd)":/go/src/github.com/tilt-dev/tilt-apiserver --workdir /go/src/github.com/tilt-dev/tilt-apiserver \
      --entrypoint ./hack/update-codegen-helper.sh \
-   golang:1.15
+   golang:1.17
