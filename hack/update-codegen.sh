22451665a3fc97f806fbf773aa7fc552c56ff610
Nick Sieger <nick@nicksieger.com>
2022-08-03 15:51:32 -0500

    codegen: update from new code-generation library
    Signed-off-by: Nick Sieger <nick@nicksieger.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -5,6 +5,8 @@ set -e
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
+goversion=$(grep "^go " go.mod | sed 's/go //')
+
 exec docker run -v "$(pwd)":/go/src/github.com/tilt-dev/tilt-apiserver --workdir /go/src/github.com/tilt-dev/tilt-apiserver \
      --entrypoint ./hack/update-codegen-helper.sh \
-   golang:1.17
+   golang:$goversion
