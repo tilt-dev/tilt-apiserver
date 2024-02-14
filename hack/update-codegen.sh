e5f5cf9b2ca1915b4c68156a3f29c100b7481137
Nick Santos <nick.santos@docker.com>
2024-02-14 16:01:39 -0500

    chore: fix up codegen privileges
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,6 +7,6 @@ cd "$DIR/.."
 
 goversion=$(grep "^go " go.mod | sed 's/go //')
 
-exec docker run -v "$(pwd)":/go/src/github.com/tilt-dev/tilt-apiserver --workdir /go/src/github.com/tilt-dev/tilt-apiserver \
+exec docker run -e "CODEGEN_UID=$(id -u)" -e "CODEGEN_GID=$(id -g)" -v "$(pwd)":/go/src/github.com/tilt-dev/tilt-apiserver --workdir /go/src/github.com/tilt-dev/tilt-apiserver \
      --entrypoint ./hack/update-codegen-helper.sh \
    golang:$goversion
