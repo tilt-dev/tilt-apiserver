469ad293e6b9b44c24934bed967f08c25db17e84
Nick Santos <nick.santos@docker.com>
2025-04-26 18:41:08 -0400

    vendor: update k8s for 1.33 (#90)
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- pkg/generated/informers/externalversions/core/v1alpha1/manifest.go
+++ pkg/generated/informers/externalversions/core/v1alpha1/manifest.go
@@ -60,13 +60,25 @@ func NewFilteredManifestInformer(client versioned.Interface, resyncPeriod time.D
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.CoreV1alpha1().Manifests().List(context.TODO(), options)
+				return client.CoreV1alpha1().Manifests().List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.CoreV1alpha1().Manifests().Watch(context.TODO(), options)
+				return client.CoreV1alpha1().Manifests().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().Manifests().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().Manifests().Watch(ctx, options)
 			},
 		},
 		&apiscorev1alpha1.Manifest{},
